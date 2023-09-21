c1bf9532dad976647bbff70d4a993db17bd5c9bb
joontae98_win <jmt2066206@gmail.com>
2023-09-21 10:35:46 +0900

    css 수정중

--- frontend/src/components/product/CategoryList.jsx
+++ frontend/src/components/product/CategoryList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Col, Row} from "react-bootstrap";
-import {ProductBox} from "../style/ProductStyle";
+import {ProductBox, ProductImg} from "../style/ProductStyle";
 
 import top from "../../static/images/category/top.jpg";
 import pants from "../../static/images/category/pants.jpg";
@@ -9,12 +9,20 @@ import ballCap from "../../static/images/category/ball_cap.jpg";
 
 const CategoryList = (props) => {
   return (<>
-    <div className={'d-flex justify-content-around'}>
-      <ProductBox src={top}>
+    <div className={'d-flex justify-content-between'}>
+      <ProductBox>
+        <ProductImg src={top}/>
       </ProductBox>
-      <ProductBox src={pants}/>
-      <ProductBox src={shoes}/>
-      <ProductBox src={ballCap}/>
+      <ProductBox>
+        <ProductImg src={pants}/>
+        <h2>test</h2>
+      </ProductBox>
+      <ProductBox>
+        <ProductImg src={shoes}/>
+      </ProductBox>
+      <ProductBox>
+        <ProductImg src={ballCap}/>
+      </ProductBox>
     </div>
   </>)
 }
--- frontend/src/components/style/ProductStyle.jsx
+++ frontend/src/components/style/ProductStyle.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from "styled-components";
 
-export const ProductBox = styled.img`
-  width: 20%;
+export const ProductImg = styled.img`
+  position: absolute;
+  width: 100%;
   margin: 1em;
   border-radius: 10px;
   //border: 0.3px solid lightgray;
   box-shadow: 1px 1px lightgray;
 `;
+
+export const ProductBox = styled.div`
+  position: relative;
+  width: 20%;
+  height: 20em;
+  `;
